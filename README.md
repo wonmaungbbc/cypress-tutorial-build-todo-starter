b0b35f3438fc5f13960dc30a1ddf27cb62146f11
Won Maung Thein <wonmaung.thein@bbc.co.uk>
2019-04-15 10:48:37 +0100

    Followed the video tutorials and added tests

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Build and test an application using Cypress
 
 This repository is the starting point for an official Cypress tutorial. We encourage you to clone this repo and follow along.
+
+To run this app
+```npm run dev```
+
+To start cypress
+```npm run cypress```
--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,13 +1,57 @@
+import { isContext } from "vm";
+
+
+
 describe("Input form", () => {
+  
+  beforeEach(() => {
+    cy.seedAndVisit([]);
+  });
   it("focuses input on load", () => {
-    cy.visit(" http://localhost:3030");
     cy.focused().should("have.class", "new-todo");
   });
-  it.only("accepts input", () => {
-    const typeText = "Bye Milk";
-    cy.visit(" http://localhost:3030");
+  it("accepts input", () => {
+    // const itemsToBuy = ["orange", "banana", "apple"];
+    // const eachItem = itemsToBuy.map(item => item);
+    const itemText = "buy banana";
+
     cy.get(".new-todo")
-      .type(typeText)
-      .should("have.value", typeText);
+      .type(itemText)
+      .should("have.value", itemText);
+  });
+
+  context("Form submission", () => {
+    beforeEach(() => {
+      cy.server();
+    });
+    it("Add a new todo on submit", () => {
+      const itemText = "Buy eggs";
+
+      cy.route("POST", "/api/todos", {
+        name: itemText,
+        id: 1,
+        isComplete: false
+      });
+      cy.get(".new-todo")
+        .type(itemText)
+        .type("{enter}")
+        .should("have.value", "");
+      cy.get(".todo-list li")
+        .should("have.length", 1)
+        .and("contain", itemText);
+      
+    });
+
+    it("Shows an error messsage on a failed submission", () => {
+      cy.route({
+        url: "/api/todos",
+        method: "POST",
+        status: 500,
+        response: {}
+      });
+      cy.get(".new-todo").type("test{enter}");
+      cy.get(".todo-list li").should("not.exist");
+      cy.get(".error").should("be.visible");
+    });
   });
 });
